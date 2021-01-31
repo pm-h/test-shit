b16a58ebe977b874ab6e39c4f9bc25e80ba1d8b0
pm_h <pillarman03@gmail.com>
2021-01-31 14:13:29 -0500

    fix and broken addition
    fixed limitLoop not letting user add string
    added broken array testing

--- main.cc
+++ main.cc
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 #include <string>
+#include <chrono>
+#include <array>
+
 
 void limitLoop() { //purely for some for loop practice
     int loopCount;
     string outLoop;
     cout << "The Loop\nEnter number of times to loop:";
     cin >> loopCount;
+    cin.get();
     cout << "And output the string: ";
     getline(cin, outLoop);
     for (int x = 1;x <= loopCount;x++) {
         cout << outLoop;
+    }
+}
+
+void arrayTest() { //broken
+    cout << "Initialize Array\n";
+    int test[3] = {1,2,3};
+    cout << "Loop Start";
+    for(int x = 0;x < (sizeof(test)/sizeof(*test));x++) {
+        cout << "X = " << x << endl;
+        cout << test[x];
     }
 }
 
@@ -22,14 +36,15 @@ int main() {
     cin >> selectItem;
         switch(selectItem){
             case 0:
-                cout << "----HELP LIST 1----\n01.............Loop\n02.............Test\n03...........Unused\n04...........Unused\n05...........Unused\n";
+                cout << "----HELP LIST 1----\n01.............Loop\n02.......Array Test\n03...........Unused\n04...........Unused\n05...........Unused\n";
                 break;
             case 1:
                 limitLoop();
                 sendBack = false;
                 break;
             case 2:
-                cout << "Test 2";
+                arrayTest;
+                sendBack = false;
                 break;
             //case 3:
         }
